2082c2f7ce00de9bc897739e5f356453a1d8a836
Leon2ev <jevgeni.glazunov@gmail.com>
2021-04-28 10:37:07 +0200

    Chnage limir alert text

--- src/components/globals/AlertLimit.vue
+++ src/components/globals/AlertLimit.vue
@@ -2,36 +2,30 @@
   <v-alert class="mt-4" border="left" text dense type="warning">
     <div v-if="isDomainsPage" class="body-1">
       Dear user, you have reached the limit of domains you can have on CIoTY
-      makerspace. Please contact support or purchase a full version of Synx Hive
-      to create your own
-      <a
-        href="https://nornir.io/our-team/"
-        target="_blank"
-        rel="noopener noreferrer"
-        >"Synx Collective"</a
-      >
+      makerspace. Please contact
+      <a href="mailto:support@nornir.io">support@nornir.io</a> or purchase a
+      full version of Synx Hive to create your own
+      <a href="https://nornir.io" target="_blank" rel="noopener noreferrer"
+        >machine collective</a
+      >.
     </div>
     <div v-if="isServicesPage" class="body-1">
       Dear user, you have reached the limit of services you can have on CIoTY
-      makerspace. Please contact support or purchase a full version of Synx Hive
-      to create your own
-      <a
-        href="https://nornir.io/our-team/"
-        target="_blank"
-        rel="noopener noreferrer"
-        >"Synx Collective"</a
-      >
+      makerspace. Please contact
+      <a href="mailto:support@nornir.io">support@nornir.io</a> or purchase a
+      full version of Synx Hive to create your own
+      <a href="https://nornir.io" target="_blank" rel="noopener noreferrer"
+        >machine collective</a
+      >.
     </div>
     <div v-if="isGhostsPage" class="body-1">
       Dear user, you have reached the limit of ghosts you can have on CIoTY
-      makerspace. Please contact support or purchase a full version of Synx Hive
-      to create your own
-      <a
-        href="https://nornir.io/our-team/"
-        target="_blank"
-        rel="noopener noreferrer"
-        >"Synx Collective"</a
-      >
+      makerspace. Please contact
+      <a href="mailto:support@nornir.io">support@nornir.io</a> or purchase a
+      full version of Synx Hive to create your own
+      <a href="https://nornir.io" target="_blank" rel="noopener noreferrer"
+        >machine collective</a
+      >.
     </div>
   </v-alert>
 </template>
