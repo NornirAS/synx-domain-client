488b96332894994e923e57110805a23f244abf90
Leon2ev <jevgeni.glazunov@gmail.com>
2021-05-20 13:39:01 +0200

    Domain form validation

--- src/components/domains/DomainForm.vue
+++ src/components/domains/DomainForm.vue
@@ -8,14 +8,14 @@
       lazy-validation
     >
       <form-input-card>
-        <div slot="title">Domain name</div>
+        <div slot="title">{{ title }}</div>
         <div slot="subtitle">
           Add a unique domain name - the name will be part of the URI address.
         </div>
         <v-text-field
           v-model="domainName"
           :rules="domainRules"
-          :counter="64"
+          :counter="maxLen"
           slot="action"
           label="Domain Name"
           prefix="https://"
@@ -41,17 +41,19 @@
 
 <script>
 import { mapState } from "vuex";
+import { requiredRule, lengthRule, urlPartRule } from "../../input-rules";
 import FormInputCard from "../globals/FormInputCard";
 export default {
   data() {
     return {
+      title: "Domain name",
+      maxLen: 63,
       domainName: "",
       valid: false,
       domainRules: [
-        v => (v && v.length) >= 1 || "Domain Name is required",
-        v =>
-          (v && v.length) <= 64 || "Domain Name must be maximum 64 characters",
-        v => /^[A-Za-z]+$/.test(v) || "Only alphabet characters are allowed"
+        v => requiredRule(v, this.title),
+        v => lengthRule(v, this.title, this.maxLen),
+        v => urlPartRule(v, this.title)
       ]
     };
   },
