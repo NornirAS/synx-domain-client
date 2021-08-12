a239afebdd88d93fbd0e36bb5fdc42b20e874339
Leon2ev <jevgeni.glazunov@gmail.com>
2021-08-12 22:37:05 +0300

    Maintanance page

---
+++ src/components/empty-page/MaintananceWork.vue
@@ -0,0 +1,44 @@
+<template>
+  <div>
+    <div align="center">
+      <v-img
+        alt="cioty"
+        src="../../assets/CIOTY-final-Logo.webp"
+        transition="scale-transition"
+        class="image-center"
+      />
+    </div>
+    <image-response-template>
+      <div slot="title">We are down for sheduled maintanance!</div>
+      <div slot="description">
+        Will be back soon!
+      </div>
+      <div slot="image">
+        <v-img
+          alt="runningWild"
+          contain
+          src="../../assets/warning.webp"
+          transition="scale-transition"
+          height="300px"
+        >
+        </v-img>
+      </div>
+    </image-response-template>
+  </div>
+</template>
+
+<script>
+import ImageResponseTemplate from "../globals/ImageResponseTemplate";
+export default {
+  components: {
+    ImageResponseTemplate
+  }
+};
+</script>
+
+<style>
+.image-center {
+  margin: 4em 0;
+  width: 100px;
+}
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,170 +1,171 @@
 import Vue from "vue";
 import VueRouter from "vue-router";
-import store from "../store";
-import Homepage from "../views/Homepage";
-import Services from "../views/Services";
-import Domains from "../views/Domains";
-import Ghosts from "../views/Ghosts";
-import Account from "../views/Account";
-import ServicesTable from "../components/services/ServicesTable";
-import ServiceForm from "../components/service/service-form/ServiceForm";
-import MicropageForm from "../components/service/micropage-form/MicropageForm";
-import SwitchTabs from "../components/service/SwitchTabs";
-import DeleteService from "../components/service/DeleteService";
-import DomainsTable from "../components/domains/DomainsTable";
-import DomainForm from "../components/domains/DomainForm";
-import AddGhosts from "../components/ghosts/AddGhost";
-import GhostsTable from "../components/ghosts/GhostsTable";
-import AboutUri from "../components/ghosts/AboutUri";
-import GhostOverview from "../views/GhostOverview";
-import SideBar from "../components/globals/SideBar";
-import PageNotFound from "../components/empty-page/PageNotFound";
+// import store from "../store";
+// import Homepage from "../views/Homepage";
+// import Services from "../views/Services";
+// import Domains from "../views/Domains";
+// import Ghosts from "../views/Ghosts";
+// import Account from "../views/Account";
+// import ServicesTable from "../components/services/ServicesTable";
+// import ServiceForm from "../components/service/service-form/ServiceForm";
+// import MicropageForm from "../components/service/micropage-form/MicropageForm";
+// import SwitchTabs from "../components/service/SwitchTabs";
+// import DeleteService from "../components/service/DeleteService";
+// import DomainsTable from "../components/domains/DomainsTable";
+// import DomainForm from "../components/domains/DomainForm";
+// import AddGhosts from "../components/ghosts/AddGhost";
+// import GhostsTable from "../components/ghosts/GhostsTable";
+// import AboutUri from "../components/ghosts/AboutUri";
+// import GhostOverview from "../views/GhostOverview";
+// import SideBar from "../components/globals/SideBar";
+// import PageNotFound from "../components/empty-page/PageNotFound";
+import MaintananceWork from "../components/empty-page/MaintananceWork";
 
 Vue.use(VueRouter);
-const authGuard = (to, from, next) => {
-  const isAuthenticated = store.getters["authentication/isAuthenticated"];
-  if (isAuthenticated) {
-    next();
-  } else {
-    next("/");
-  }
-};
+// const authGuard = (to, from, next) => {
+//   const isAuthenticated = store.getters["authentication/isAuthenticated"];
+//   if (isAuthenticated) {
+//     next();
+//   } else {
+//     next("/");
+//   }
+// };
 
-const skipHomeIfAuth = (to, from, next) => {
-  const isAuthenticated = store.getters["authentication/isAuthenticated"];
-  if (isAuthenticated) {
-    next("/services");
-  } else {
-    next();
-  }
-};
+// const skipHomeIfAuth = (to, from, next) => {
+//   const isAuthenticated = store.getters["authentication/isAuthenticated"];
+//   if (isAuthenticated) {
+//     next("/services");
+//   } else {
+//     next();
+//   }
+// };
 
 const routes = [
-  {
-    path: "/",
-    name: "home",
-    components: {
-      default: Homepage
-    },
-    beforeEnter: skipHomeIfAuth
-  },
-  {
-    path: "/domains",
-    components: {
-      default: Domains,
-      "side-bar": SideBar
-    },
-    children: [
-      {
-        path: "",
-        name: "domains",
-        component: DomainsTable,
-        meta: {
-          title: "Domains"
-        }
-      },
-      {
-        path: "create-domain",
-        name: "create-domain",
-        component: DomainForm,
-        meta: {
-          title: "New Domain"
-        }
-      }
-    ],
-    beforeEnter: authGuard
-  },
-  {
-    path: "/services",
-    components: {
-      default: Services,
-      "side-bar": SideBar
-    },
-    children: [
-      {
-        path: "",
-        name: "services",
-        component: ServicesTable,
-        meta: {
-          title: "Morphic Services"
-        }
-      },
-      {
-        path: "service-create",
-        name: "serviceCreate",
-        components: {
-          default: ServiceForm
-        },
-        meta: {
-          title: "New Morphic Service"
-        }
-      },
-      {
-        path: ":serviceName/update",
-        name: "serviceUpdate",
-        components: {
-          default: ServiceForm,
-          "switch-tabs": SwitchTabs,
-          "delete-service": DeleteService
-        },
-        meta: {
-          title: "Update Morphic Service"
-        }
-      },
-      {
-        path: ":serviceName/update/micropage",
-        name: "micropageUpdate",
-        components: {
-          default: MicropageForm,
-          "switch-tabs": SwitchTabs
-        },
-        meta: {
-          title: "Update Micropage"
-        }
-      }
-    ],
-    beforeEnter: authGuard
-  },
-  {
-    path: "/ghosts",
-    components: {
-      default: Ghosts,
-      "side-bar": SideBar
-    },
-    children: [
-      {
-        path: "",
-        name: "ghosts",
-        components: {
-          default: AddGhosts,
-          "ghosts-list": GhostsTable,
-          "side-right": AboutUri
-        },
-        meta: {
-          title: "Ghosts"
-        }
-      },
-      {
-        path: ":domain/:service##:instance",
-        name: "ghostOverview",
-        component: GhostOverview,
-        meta: {
-          title: "Ghost Details Overview:"
-        }
-      }
-    ],
-    beforeEnter: authGuard
-  },
-  {
-    path: "/account",
-    name: "account",
-    components: {
-      default: Account,
-      "side-bar": SideBar
-    },
-    beforeEnter: authGuard
-  },
-  { path: "*", component: PageNotFound }
+  // {
+  //   path: "/",
+  //   name: "home",
+  //   components: {
+  //     default: Homepage
+  //   },
+  //   beforeEnter: skipHomeIfAuth
+  // },
+  // {
+  //   path: "/domains",
+  //   components: {
+  //     default: Domains,
+  //     "side-bar": SideBar
+  //   },
+  //   children: [
+  //     {
+  //       path: "",
+  //       name: "domains",
+  //       component: DomainsTable,
+  //       meta: {
+  //         title: "Domains"
+  //       }
+  //     },
+  //     {
+  //       path: "create-domain",
+  //       name: "create-domain",
+  //       component: DomainForm,
+  //       meta: {
+  //         title: "New Domain"
+  //       }
+  //     }
+  //   ],
+  //   beforeEnter: authGuard
+  // },
+  // {
+  //   path: "/services",
+  //   components: {
+  //     default: Services,
+  //     "side-bar": SideBar
+  //   },
+  //   children: [
+  //     {
+  //       path: "",
+  //       name: "services",
+  //       component: ServicesTable,
+  //       meta: {
+  //         title: "Morphic Services"
+  //       }
+  //     },
+  //     {
+  //       path: "service-create",
+  //       name: "serviceCreate",
+  //       components: {
+  //         default: ServiceForm
+  //       },
+  //       meta: {
+  //         title: "New Morphic Service"
+  //       }
+  //     },
+  //     {
+  //       path: ":serviceName/update",
+  //       name: "serviceUpdate",
+  //       components: {
+  //         default: ServiceForm,
+  //         "switch-tabs": SwitchTabs,
+  //         "delete-service": DeleteService
+  //       },
+  //       meta: {
+  //         title: "Update Morphic Service"
+  //       }
+  //     },
+  //     {
+  //       path: ":serviceName/update/micropage",
+  //       name: "micropageUpdate",
+  //       components: {
+  //         default: MicropageForm,
+  //         "switch-tabs": SwitchTabs
+  //       },
+  //       meta: {
+  //         title: "Update Micropage"
+  //       }
+  //     }
+  //   ],
+  //   beforeEnter: authGuard
+  // },
+  // {
+  //   path: "/ghosts",
+  //   components: {
+  //     default: Ghosts,
+  //     "side-bar": SideBar
+  //   },
+  //   children: [
+  //     {
+  //       path: "",
+  //       name: "ghosts",
+  //       components: {
+  //         default: AddGhosts,
+  //         "ghosts-list": GhostsTable,
+  //         "side-right": AboutUri
+  //       },
+  //       meta: {
+  //         title: "Ghosts"
+  //       }
+  //     },
+  //     {
+  //       path: ":domain/:service##:instance",
+  //       name: "ghostOverview",
+  //       component: GhostOverview,
+  //       meta: {
+  //         title: "Ghost Details Overview:"
+  //       }
+  //     }
+  //   ],
+  //   beforeEnter: authGuard
+  // },
+  // {
+  //   path: "/account",
+  //   name: "account",
+  //   components: {
+  //     default: Account,
+  //     "side-bar": SideBar
+  //   },
+  //   beforeEnter: authGuard
+  // },
+  { path: "*", component: MaintananceWork }
 ];
 
 const router = new VueRouter({
